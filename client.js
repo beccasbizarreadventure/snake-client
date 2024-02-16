4f0ca30fe6ad986dbca4d14985e8e6d656af7199
Rebecca <rebecca90469@gmail.com>
2024-02-16 11:20:36 -0500

    Moved connect function to client.js and exported connect to be used by play.js

---
+++ client.js
@@ -0,0 +1,26 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = () => {
+  const conn = net.createConnection({
+    host: "172.29.64.193",
+    port: 50541,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on("connect", () => {
+    console.log("Ready to play!");
+  });
+
+  conn.on("data", (eventData) => {
+    console.log(eventData);
+  });
+
+  return conn;
+};
+
+module.exports = {
+  connect
+};
