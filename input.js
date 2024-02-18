04e6ce9fb2529c808f954dbae3e361954d30df86
Rebecca <rebecca90469@gmail.com>
2024-02-18 13:58:56 -0500

    Created seperate file for input handling function and exported function

---
+++ input.js
@@ -0,0 +1,41 @@
+let player = {
+  x: 0,
+  y: 0,
+};
+
+const handleUserInput = function(data) {
+  
+  if (data === '\u0003') {
+    process.exit(); 
+  }
+  if (data === 'w') {
+    player.y += 1;
+    console.log(`Position: x = ${player.x}, y = ${player.y}`);
+  }
+  if (data === 's') {
+    player.y -= 1;
+    console.log(`Position: x = ${player.x}, y = ${player.y}`);
+  }
+  if (data === 'a') {
+    player.x -=1;
+    console.log(`Position: x = ${player.x}, y = ${player.y}`);
+  }
+  if (data === 'd') {
+    player.x += 1;
+    console.log(`Position: x = ${player.x}, y = ${player.y}`);
+  }
+
+};
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+module.exports = {
+  setupInput
+};
